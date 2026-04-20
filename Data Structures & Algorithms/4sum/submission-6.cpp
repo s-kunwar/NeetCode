8701ab3049b309c7023dee3bf37a271c550d1d8b
Suraj Kunwar <surajkunwar110907@gmail.com>
2026-04-20 11:19:55 +0530

    Add: 4sum - submission-6

---
+++ Data Structures & Algorithms/4sum/submission-6.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+typedef long long ll;
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        int n=nums.size();
+        if (n < 4) return {};
+        //first fixed value
+        for(int i=0;i<=(n-4);i++){
+            //second fixed value
+            if (i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<=(n-3);j++){
+                int l=j+1,r=n-1;
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                while(l<r){
+                    ll sum=(ll)nums[i]+(ll)nums[j]+(ll)nums[l]+(ll)nums[r];
+                    if(sum==target){
+                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
+                        while(l<r && nums[l]==nums[l+1]) l++;
+                        while(l<r && nums[r]==nums[r-1]) r--;
+                        l++;
+                        r--;
+                    }
+                    else if(sum<target) l++;
+                    else r--;
+                }
+            }
+        }
+        return ans;
+    }
+};
