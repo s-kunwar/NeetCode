457e077fba280a5ad329e205d8bc71be03dcee55
Suraj Kunwar <surajkunwar110907@gmail.com>
2026-08-20 23:02:44 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    // Direction vectors for moving Up, Down, Left, Right
+    vector<int>dr= {-1, 1, 0, 0};
+    vector<int>dc= {0, 0, -1, 1};
+
+    bool isValid(int r, int c, int rows, int cols, const vector<vector<char>>& grid, const      vector<vector<bool>>& visited) {
+        return (r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] == '1' && !visited[r][c]);
+    }
+
+    void dfs(int r, int c, const vector<vector<char>>& grid, vector<vector<bool>>& visited) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        visited[r][c] = true;
+
+        for (int i = 0; i < 4; ++i) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if (isValid(nr, nc, rows, cols, grid, visited))dfs(nr, nc, grid, visited);
+        }
+    }
+
+public:
+
+    int numIslands(vector<vector<char>>& grid) {
+        
+        vector<vector<bool>> visited (grid.size(),vector<bool>(grid[0].size(),false));
+        int sum=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if( isValid(i,j,grid.size(),grid[0].size(),grid,visited) ){dfs(i,j,grid,visited);sum++;}
+            }
+        }
+
+        return sum;
+
+    }
+
+
+};
